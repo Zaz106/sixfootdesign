8e5a0e736aec24810c3d1e56616236b1c74f0736
Joshua Huisman <St10471178@imconnect.edu.za>
2026-01-27 13:23:18 +0200

    Updated home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const bebasNeue = Bebas_Neue({
@@ -31,6 +32,7 @@ export default function RootLayout({
     <html lang="en" className={`${bebasNeue.variable} ${poppins.variable}`}>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
